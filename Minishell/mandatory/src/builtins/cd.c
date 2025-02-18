cdb58a1c0cfae898981e95dbe4489e793cb40334
Christophe Donnat <chdonnat@student.42perpignan.fr>
2025-02-18 16:39:43 +0100

    working on builtins: cd.c and pwd.c seems ok

--- Minishell/mandatory/src/builtins/cd.c
+++ Minishell/mandatory/src/builtins/cd.c
@@ -5,63 +5,58 @@ COMPILE WITH:
 gcc -o test_cd -Wall -Werror -Wextra cd.c ../../../dclst/dclst1.c ../../../dclst/dclst2.c ../../../dclst/dclst3.c -L../../../libft -lft_inc -I../../../libft/headers/libft_H
 *****************************************************************************/
 
-// Function to get an environement variable from the key
-char	*get_env_value(const char *key, char **envp)
+// Function to change directory to the parent directory
+int	go_to_parent_dir(void)
 {
-	int		i;
-	size_t	len;
-	char	*value;
+	char	pwd[PATH_MAX];
+	char	*ptr;
 
-	len = ft_strlen(key);
-	i = 0;
-	while (envp[i])
+	if (!getcwd(pwd, sizeof(pwd)))
+		return (perror("cd: getcwd failed"), -1);
+	ptr = ft_strrchr(pwd, '/');
+	if (ptr == NULL)
+		return (perror("cd: invalid path"), -1);
+	if (ptr == pwd)
 	{
-		if (ft_strncmp(key, envp[i], len) == 0 && envp[i][len] == '=')
-		{
-			value = envp[i] + len + 1;
-			return (value);
-		}
-		i++;
+		if (chdir("/") == -1)
+			return (perror("cd error"), -1);
 	}
-	return (NULL);
+	else
+	{
+		*ptr = '\0';
+		if (chdir(pwd) == -1)
+			return (perror("cd error"), -1);
+	}
+	return (0);
 }
 
 // Function to change directory
 // like the cd command
-int	cd_cmd(const char *path, char **envp)
+int	cd_cmd(const char *path)
 {
 	if (!path || *path == '\0')
-		path = get_env_value("HOME", envp);
+		path = getenv("HOME");
 	if (!path)
 		return (ft_printf("cd: HOME NOT SET "), -1);
-	if (!ft_strncomp(path, "..", 2) && ft_strlen(path) == 2)
-		path = get_env_value("OLDPWD", envp);
-	else if (!ft_strncomp(path, ".", ft_strlen(path)))
+	if (!ft_strncmp(path, "..", 2) && ft_strlen(path) == 2)
+		return (go_to_parent_dir());
+	else if (!ft_strncmp(path, ".", 1) && ft_strlen(path) == 1)
 		return (0);
-	else if (!ft_strncomp(path, "~", 1) && ft_strlen(path) == 1)
+	else if (!ft_strncmp(path, "~", 1) && ft_strlen(path) == 1)
 	{
 		if (chdir("/") == -1)
-		{
-			if (chdir(path) == -1)
-				return (perror("cd error"), -1);
-			return (0);
-		}
+			return (perror("cd error"), -1);
+		return (0);
 	}
 	if (chdir(path) == -1)
 		return (perror("cd error"), -1);
 	return (0);
 }
 
-int	main(int ac, char **av, char **envp)
+int	main(int ac, char **av)
 {
 	if (ac != 2)
 		return (ft_printf("one arg needed"), 1);
-	// char	*res;
-	// res = get_env_value(av[1], envp);
-	// if (!res)
-	// 	ft_printf("env_var not found\n");
-	// else
-	// 	ft_printf("%s\n", res);
-	cd_cmd(av[1], envp);
+	cd_cmd(av[1]);
 	return (0);
 }
