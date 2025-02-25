93eb8797eeee49a272e0f504095b2312fd85b4b1
chdonnat <donnatchris@live.fr>
2025-02-25 11:17:33 +0100

    working on multiple pipes

--- Minishell/mandatory/src/main.c
+++ Minishell/mandatory/src/main.c
@@ -18,19 +18,19 @@ int	main(int ac, char **av, char **envp)
 		gen->head = tokenize(gen->input);
 		if (check_syntax(gen->head) == -1)
 			continue ;
+		t_tree *tree = create_tree(*gen->head, (*gen->head)->prev->prev);
 
 		// to test a line of command without any operator:
 		// exec_node(*gen->head, &gen->envp, gen);
 
 		// to see the created tokens:
-		ft_printf("\nPrinting tokens:\n");
-		print_dclst_tokens(gen->head);
+		// ft_printf("\nPrinting tokens:\n");
+		// print_dclst_tokens(gen->head);
 
 		// to see the created tree:
-		t_tree *tree = create_tree(*gen->head, (*gen->head)->prev->prev);
-		ft_printf("\nPrinting tree:\n");
-		print_tree(tree);
-
+		// ft_printf("\nPrinting tree:\n");
+		// print_tree(tree);
+		
 		// to test the tree execution:
 		exec_tree(tree, &gen->envp, gen);
 	}
