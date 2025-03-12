d7e5597769c47f83d4f4c0debfd0b07f7c10c8e3
chdonnat <donnatchris@live.fr>
2025-03-12 23:05:13 +0100

    added deepseek heredoc to test

--- Minishell/mandatory/src/operators/heredoc.c
+++ Minishell/mandatory/src/operators/heredoc.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   heredoc.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: chdonnat <chdonnat@student.42.fr>          +#+  +:+       +#+        */
+/*   By: christophedonnat <christophedonnat@stud    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/07 04:52:40 by christophed       #+#    #+#             */
-/*   Updated: 2025/03/12 10:38:28 by chdonnat         ###   ########.fr       */
+/*   Updated: 2025/03/12 23:04:33 by christophed      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -104,3 +104,99 @@ int	redir_heredoc(t_dclst *node, t_general *gen)
 		return (close(pipefd[0]), ft_perror("redir_heredoc", "dup2 failed"));
 	return (close(pipefd[0]), 0);
 }
+
+
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <unistd.h>
+// #include <fcntl.h>
+// #include <string.h>
+// #include <readline/readline.h>
+// #include <readline/history.h>
+
+// #define TMP_FILE "/tmp/minishell_heredoc.tmp"
+
+// // Fonction pour créer un fichier temporaire contenant l'entrée du heredoc
+// int create_heredoc(const char *delimiter) {
+//     int fd;
+//     char *line;
+
+//     // Ouvrir le fichier temporaire en écriture
+//     fd = open(TMP_FILE, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+//     if (fd == -1) {
+//         perror("open");
+//         return -1;
+//     }
+
+//     // Lire l'entrée utilisateur jusqu'à ce que le délimiteur soit rencontré
+//     while (1) {
+//         line = readline("heredoc> ");
+//         if (!line) {
+//             // Ctrl+D détecté
+//             printf("warning: end-of-file detected (delimiter '%s' not found)\n", delimiter);
+//             break;
+//         }
+//         if (strcmp(line, delimiter) == 0) {
+//             free(line);
+//             break;
+//         }
+//         dprintf(fd, "%s\n", line); // Écrire la ligne dans le fichier temporaire
+//         free(line);
+//     }
+
+//     close(fd);
+//     return 0;
+// }
+
+// // Fonction pour rediriger l'entrée standard vers le fichier temporaire
+// int redirect_heredoc() {
+//     int fd;
+
+//     // Ouvrir le fichier temporaire en lecture
+//     fd = open(TMP_FILE, O_RDONLY);
+//     if (fd == -1) {
+//         perror("open");
+//         return -1;
+//     }
+
+//     // Rediriger STDIN_FILENO vers le fichier temporaire
+//     if (dup2(fd, STDIN_FILENO) == -1) {
+//         perror("dup2");
+//         close(fd);
+//         return -1;
+//     }
+
+//     close(fd);
+//     return 0;
+// }
+
+// // Fonction pour nettoyer le fichier temporaire
+// void cleanup_heredoc() {
+//     unlink(TMP_FILE); // Supprimer le fichier temporaire
+// }
+
+// // Exemple d'utilisation dans une commande
+// int main() {
+//     const char *delimiter = "EOF";
+
+//     // Créer le heredoc
+//     if (create_heredoc(delimiter) {
+//         fprintf(stderr, "Erreur lors de la création du heredoc.\n");
+//         return 1;
+//     }
+
+//     // Rediriger l'entrée standard vers le heredoc
+//     if (redirect_heredoc()) {
+//         fprintf(stderr, "Erreur lors de la redirection du heredoc.\n");
+//         cleanup_heredoc();
+//         return 1;
+//     }
+
+//     // Exécuter une commande (par exemple, `cat`)
+//     execlp("cat", "cat", NULL);
+
+//     // Nettoyer le fichier temporaire
+//     cleanup_heredoc();
+
+//     return 0;
+// }
